d6e0e1a098624f9e8cdcd19b4eca0fbed08c70fa
Nicolás López <nicolaslopezj@me.com>
2015-08-27 12:50:12 -0300

    add medium schema

---
+++ schema/collections/mediums/_init.js
@@ -0,0 +1,9 @@
+Mediums = new orion.collection('mediums', {
+  pluralName: 'Medios',
+  singularName: 'Medio',
+  tabular: {
+    columns: [
+      { data: 'name', title: 'Nombre' },
+    ]
+  }
+});
