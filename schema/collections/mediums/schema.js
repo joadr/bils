3a33873a6ea8dc3fb7b16b47e81a32ce35d9fabc
Nicolás López <nicolaslopezj@me.com>
2015-08-27 14:41:24 -0300

    ordenate joquin

--- schema/collections/mediums/schema.js
+++ schema/collections/mediums/schema.js
@@ -38,98 +38,82 @@ Mediums.attachSchema({
   //   titleField: 'name',
   //   publicationName: 'mediumsCity',
   // }),
-
   name: {
     type: String,
     label: 'Nombre',
     optional: true
   },
-
   relevance: {
     type: Number,
     label: 'Relevancia',
     allowedValues: [1,2,3,4,5],
     optional: true
   },
-
   siteViews: {
     type: Number,
     label: "Tiraje",
     optional: true
   },
-
   dailyViews: {
     type: Number,
     label: "Lectoría / Diaria",
     optional: true
   },
-
   weekendViewsFactor: {
     type: Number,
     decimal: true,
     label: "Factor lectoría fin de semana"
   },
-
   frecuency: {
     type: String,
     label: "Frecuencia",
     optional: true
   },
-
   targetMarket: {
     type: String,
     label: 'Publico objetivo',
     optional: true
   },
-
   monthlyViews: {
     type: Number,
     label: 'Visitas mensuales (solo web)',
     optional: true
   },
-
   value: {
     type: String,
     label: 'Valor',
     optional: true
   },
-
   valueFullPage: {
     type: String,
     label: 'Valor página entera',
     optional: true
   },
-
   'value34Page': {
     type: String,
     label: 'Valor 3/4 página',
     optional: true
   },
-
   valueNarrowHalfPage: {
     type: String,
     label: 'Valor media página alto',
     optional: true
   },
-
   valueWideHalfPage: {
     type: String,
     label: 'Valor media página tradicional',
     optional: true
   },
-
   'value14Page': {
     type: String,
     label: 'Valor 1/4 página',
     optional: true
   },
-
   valueMediumSpot: {
     type: String,
     label: 'Valor caluga mediana',
     optional: true
   },
-
   valueSmallSpot: {
     type: String,
     label: 'Valor caluga pequeña',
@@ -140,7 +124,6 @@ Mediums.attachSchema({
     label: 'Valor banner horizontal',
     optional: true
   },
-
   valueVerticalBanner: {
     type: String,
     label: 'Valor banner vertical',
