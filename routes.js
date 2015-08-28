1060e7ad226db19fdea99796c2499e00489a8773
Joadr <joaquin.diaz@ies.cl>
2015-08-28 00:46:03 -0300

    pdf generation

--- routes.js
+++ routes.js
@@ -1,7 +1,30 @@
 Router.route('/admin/export/news/:filename', function () {
-  var doc = ExportNews.findOne(this.params.filename);
-  return doc.fileType;
-  //this.response.end(this.params.filename+'\n');
+  var row = ExportNews.findOne(this.params.filename);
+  ExportNews.remove(this.params.filename);
+  // this.response.end(doc.fileType+'\n');
+  var doc = new PDFDocument({size: 'A4', margin: 50});
+  doc.fontSize(12);
+  doc.text('Hola mundo', 10, 30, {align: 'center', width: 200});
+  http.get('http://whizzy.azurewebsites.net/files/whizzy-logo-text.png').on('response', function(res){
+      res.setEncoding('binary');
+      res.on('data', function(chunk){
+         buffer += chunk;
+      });
+      res.on('end', function(){
+        doc.image(buffer, 0, 15, { width: 300 }).text('Proprotional to width', 0, 0);
+       //After file is download and was write into the HD will use it
+
+     }).end(); //EO Downloading the file
+  });
+
+
+  // doc.image('http://whizzy.azurewebsites.net/files/whizzy-logo-text.png', 0, 15, { width: 300 }).text('Proprotional to width', 0, 0);
+
+  this.response.writeHead(200, {
+    'Content-type': 'application/pdf',
+    'Content-Disposition': "attachment; filename=test.pdf"
+  });
+  this.response.end( doc.outputSync() );
 }, {name: 'news.export.file', where: 'server'});
 
 Router.route('/admin/export/news',  {
