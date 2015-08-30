de95216f185d28534fe130b875de6dd27a536db6
Joadr <joaquin.diaz@ies.cl>
2015-08-30 17:12:43 -0300

    pdf casi finished

--- routes.js
+++ routes.js
@@ -1,24 +1,49 @@
 Router.route('/admin/export/news/:exportable', function () {
   var row = ExportNews.findOne(this.params.exportable);
-  var exportable = row;
-  ExportNews.remove(this.params.exportable);
-  // this.response.end(doc.fileType+'\n');
-  if(exportable.fileType == 'pdf'){
-    var result = Meteor.call("exportPDF", exportable);
+  exportable = row;
 
-    this.response.writeHead(200, {
-      'Content-type': 'application/pdf',
-      'Content-Disposition': "attachment; filename=test.pdf"
+  var doc = new PDFDocument({size: 'A4', margin: 50});
+  doc.fontSize(12);
+
+
+  var news = News.find({groupId: {$in: exportable.groupsIds }, brandId: {$in: exportable.brandsIds} }).fetch();
+
+  var a = 400;
+  news.forEach(function(element, index, array){
+    // Logo is inserted on every page
+    var result = request.getSync('http://whizzy.azurewebsites.net/files/whizzy-logo-text.png', {
+        encoding: null
     });
-    this.response.end(result);
-  } else if(exportable.fileType == 'xls'){
-    // para excel
-  } else if(exportable.fileType == 'ppt'){
-    // para powerpoint
-  }
+    var buffer = result.body;
 
+    doc.image(buffer, 400, 10, { width: 100 });
 
+    // we add a page per new
+    if(index != 0){
+      doc.addPage();
+    }
 
+    // we put the new photo
+    var result = request.getSync(element.media[0].url, {
+      encoding: null
+    });
+    var buffer = result.body;
+
+    doc.image(buffer, 10, 60, { width: 580, height: 580 });
+
+    // the new data
+    doc.text("TITULO: "+element.title, 10, 650);
+    doc.text("SUPLEMENTO: "+element.suplement);
+    doc.text("FECHA: "+element.date);
+    doc.text("CENTIMETRAJE: "+element.size);
+    doc.text("VALOR APROX: "+element.title);
+  });
+
+  this.response.writeHead(200, {
+    'Content-type': 'application/pdf',
+    'Content-Disposition': "attachment; filename=test.pdf"
+  });
+  this.response.end( doc.outputSync() );
 }, {name: 'news.export.file', where: 'server'});
 
 Router.route('/admin/export/news',  {
--- server/export/pdf.js
+++ server/export/pdf.js
@@ -16,27 +16,28 @@ Meteor.methods({
 
     // then we put all the data in the file
     // for images
-    var images = [];
-    for(key in news){
-      if(key != 0){
-        // we add a new page per new
-        doc.addPage();
-      }
-
-      // we put the image
-      console.log(news[key].media[0].url);
-      images.push(request.getSync('http://whizzy.azurewebsites.net/files/whizzy-logo-text.png', {
-          encoding: null
-      }));
-      // http://localhost:3000/gridfs/data/id/5263c1ffdc1b6e10fa24f3a6
-
-      //var buffer = image.body;
-      doc.image(images[images.length-1].body, 0, 50);
-
-      // then the content:
-      doc.text('TITULO: '+news[key].title).text('SUPLEMENTO: '+news[key].suplement).text('FECHA: '+news[key].date).text('CENTIMETRAJE: '+news[key].size);
-
-    }
+    // var images = [];
+    // for(key in news){
+    //   console.log(news[key]);
+    //   if(key != 0){
+    //     // we add a new page per new
+    //     doc.addPage();
+    //   }
+    //
+    //   // we put the image
+    //   console.log(news[key].media[0].url);
+    //   images.push(request.getSync('http://whizzy.azurewebsites.net/files/whizzy-logo-text.png', {
+    //       encoding: null
+    //   }));
+    //   // http://localhost:3000/gridfs/data/id/5263c1ffdc1b6e10fa24f3a6
+    //
+    //   //var buffer = image.body;
+    //   doc.image(images[images.length-1].body, 0, 50);
+    //
+    //   // then the content:
+    //   doc.text('TITULO: '+news[key].title).text('SUPLEMENTO: '+news[key].suplement).text('FECHA: '+news[key].date).text('CENTIMETRAJE: '+news[key].size);
+    //
+    // }
 
     return doc.outputSync();
   }
