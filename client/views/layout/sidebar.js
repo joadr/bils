83c93d2f41cc08676cc76bad4eae396f60b111c2
Nicolás López <nicolaslopezj@me.com>
2015-08-31 13:20:21 -0300

    logo para todos excepto admin

--- client/views/layout/sidebar.js
+++ client/views/layout/sidebar.js
@@ -1,5 +1,8 @@
 Template.sidebar.helpers({
   agency: function() {
     return Agencies.findOne();
+  },
+  showAgencyLogo: function() {
+    return _.contains(Meteor.user().roles(), 'agencia') || _.contains(Meteor.user().roles(), 'ejecutivo') || _.contains(Meteor.user().roles(), 'cliente')
   }
 })
